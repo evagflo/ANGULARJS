22e383aada934084125cec6bbb4bd12dfd95d8f7
evagflo <evagonzalezflo@gmail.com>
2021-04-19 13:26:21 +0200

    module 1

---
+++ module1/app.js
@@ -0,0 +1,28 @@
+(function (){
+    'use strict'; //not do mistakes
+
+    angular.module('NameCalculator', [])
+
+    .controller('NameCalculatorController', function($scope){
+    $scope.name= "";
+    $scope.totalValue= 0;  
+
+
+    $scope.displayNumeric = function(){
+        var totalNameValue=calculateNumericForString ($scope.name);//gets de total value;
+        $scope.totalValue=totalNameValue;
+    };
+
+    function calculateNumericForString(string){
+        var totalStringValue = 0;
+        for ( var i = 0; i< string.length; i++) {
+            totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
+
+    }
+
+    });
+  
+    
+})();
